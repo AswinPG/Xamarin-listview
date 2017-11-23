caf31496e0f9dab10464edd05351e388bd150bb2
Aswin P G <aswinpg4444@outlook.com>
2017-11-23 07:04:10 -0800

    Is there anything wrong in the code?
    Cannot view the elements in the listview.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+            InitializeComponent();
+            MainListView.ItemsSource = new List<Person>
+            {
+                new Person
+                {
+                    Name="Aswin",
+                    Age=18
+                },
+
+
+                new Person
+                {
+                    Name="Anjana",
+                    Age=18
+                },
+
+                new Person
+                {
+                    Name="Anjana and Aswin",
+                    Age=18
+                },
+
+                new Person
+                {
+                    Name="Anju",
+                    Age=15
+                }
+            };
+		}
+	}
+}
